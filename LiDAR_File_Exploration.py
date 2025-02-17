54f3ac258ee965defed7bedbf6626c1552a0d55a
Spenser <spenseranderson2@gmail.com>
2025-02-16 22:07:28 -0500

    Cropped the Las and added Z-score cutoff

--- LiDAR_File_Exploration.py
+++ LiDAR_File_Exploration.py
@@ -24,26 +24,37 @@ las_fname = (Path.cwd()).parent.parent.joinpath(r'USGS_LPC_FL_Peninsular_FDEM_20
 #%% Opening the las data
 ##Reading all data
 las_df = laspy.read(las_fname)
-proj = CRS(6437)
+
+#Figuring out the projection
+las_crs = CRS.from_wkt(str(las_df.header.parse_crs())).sub_crs_list[0].to_epsg()
+proj = las_crs
+
 ##Reading only the ground return points
 las_ground_df = las_df.points[las_df.classification==2] #where 2 is ground
 
 #Extent
-las_df_extent = [las_df.header.min,las_df.header.max]
-print(las_df_extent)    #[minx, miny, minz], [maxx, maxy, maxz]
+las_df_extent = [las_df.x.min,las_df.x.max,
+                 las_df.y.min, las_df.y.max ]
+# print(las_df_extent)    #[minx, miny, minz], [maxx, maxy, maxz]
 
-## Enpty Array
-las_array = np.vstack((las_ground_df.x, las_ground_df.y, las_ground_df.z)).transpose()
+
+#Checking CRS
+# las_df.header.parse_crs()
 
 
 #%% Create Shapefile to Cut Las Dataset
 start = time.time()
-## Reading the box
+## Formating the bounds
+#Reading the box
 fishnet_fname = (Path.cwd()).parents[1].joinpath(r'Emmett_Reed_Fishnet.json')
 box = gp.read_file(fishnet_fname).dissolve()
+#Projecting the box
 box_coord_syst = box.crs
-print(box_coord_syst)
+
+#Getting bounds
+minx, miny, maxx, maxy = box.total_bounds
 box_np = box.to_numpy()
+
 
 #Checking Coordinate System
 if proj == box_coord_syst:
@@ -71,14 +82,50 @@ else:
         las_y[i:i + chunk_size] = projected_y
         las_z[i:i + chunk_size] = projected_z
 
-        #Replacing vals
-        las_ground_df.x = las_x
-        las_ground_df.y = las_y
-        las_ground_df.z = las_z
+    #Replacing vals
+    las_ground_df.x = las_x
+    las_ground_df.y = las_y
+    las_ground_df.z = las_z
 
-    # Transform the coordinates
-    lat, lon, elev = transformer.transform(las_x, las_y, las_z) 
+    # # Transform the coordinates
+    # lat, lon, elev = transformer.transform(las_x, las_y, las_z) 
+
+##Bools for Values
+x_valid = (las_ground_df.x >= minx) & (las_ground_df.x <= maxx)
+y_valid = (las_ground_df.y >= miny) & (las_ground_df.y <= maxy)
+
+#combined Bools
+valid_points = x_valid & y_valid
+
+## Clipping and Converting to Numpy Array
+#Creating array
+las_array = np.vstack((las_ground_df.x, las_ground_df.y, las_ground_df.z)).transpose()
+
+#Clipping 
+las_array_clipped = las_array[valid_points]
 end = time.time()
 print(f'Elapsed Time {end-start}')
 
+#%% Working with the Cropped LAS file
+##What are some statistics on the Elevation?
+#Numpy Array
+las_elev_array = las_array_clipped[:,2]
 
+##Stats
+#3 num summary
+min_elev = np.min(las_elev_array)
+mean_elev = np.average(las_elev_array)
+max_elev = np.max(las_elev_array)
+sd = np.std(las_elev_array)
+
+#Quartiles
+perc_5 = np.percentile(las_elev_array, 1)
+q1 = np.percentile(las_elev_array, 25)
+
+#A 3 Z-score for outside the 97.5%
+threshold = ((-3*sd) + mean_elev)
+
+
+#%% Tag Points Greater than 75th percentile
+#Cloud points greater than the q4 value
+las_array_filt = las_array_clipped[np.where(las_array_clipped[:,2] < threshold)]
